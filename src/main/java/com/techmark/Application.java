2261b9aeed192627807b91bd214a91779fd58f15
Gurpreet Singh <Gurpreet.Singh@talentica.com>
2018-06-29 20:18:52 +0530

    Disable default security configuration

--- src/main/java/com/techmark/Application.java
+++ src/main/java/com/techmark/Application.java
@@ -3,11 +3,14 @@ package com.techmark;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.techmark")
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class, JmxAutoConfiguration.class,DataSourceAutoConfiguration.class })
 public class Application {
 
     public static void main(String[] args) {
